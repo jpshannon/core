2ca722ee3e21b4bcbe1e22a290742ca6cfae4502
Joshua Shannon <jshannon@ark.org>
2015-05-11 17:53:03 -0500

    Added config property to Console to easy code transitions

--- src/Console.php
+++ src/Console.php
@@ -3,10 +3,26 @@ namespace \werx\Core;
 
 class Console
 {
+	/**
+	 * @var \werx\Core\WebAppContext $config
+	 */
+	public $config;
+
+	/**
+	 * @var \werx\Core\WebAppContext $context
+	 */
+	public $context;
+
+	/**
+	 * @var \werx\Core\WerxApp $app
+	 */
+	public $app;
+
 	public function __construct($context, $cli_only = true)
 	{
 		$this->app = $context->getApp();
 		$this->context = $context;
+		$this->config = $context; // help transition;
 		if ($cli_only && $this->context->cli === false) {
 			die("This is only available from CLI.\n");
 		}
