f5bc2caedc5d05562ec67514bb31bb49154412b2
Joshua Shannon <jshannon@ark.org>
2015-05-11 15:18:01 -0500

    Choose were to init the database from

--- src/Modules/Database.php
+++ src/Modules/Database.php
@@ -8,10 +8,12 @@ use \werx\Core\Database;
 
 class Database extends Module
 {
+	public function __construct($config = 'database', $item = 'default')
+
 	public function handle(WerxApp $app)
 	{
-		$app->config->load('database', true);
-		Database::init($app->config->database('default'));
+		$app->config->load($config, true);
+		Database::init($app->config->get($item, null, $config));
 		$this->handleNext($app);
 	}
 }
